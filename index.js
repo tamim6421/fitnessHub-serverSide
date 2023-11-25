b988659851618712c26820a01460f7c920c25c32
ProgrammingHero <tamimhossain6421@gmail.com>
2023-11-25 22:22:21 +0600

    post jwt token and verifiy user to middlewere and also added the verify admin

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-
+const jwt = require('jsonwebtoken');
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000 
-
 
 app.use(cors())
 app.use(express.json())
@@ -32,6 +32,55 @@ const forumPostCollection = client.db("fitnessHub").collection('posts')
 const subscriberCollection = client.db("fitnessHub").collection('subscribe')
 const confirmTrainerCollection = client.db("fitnessHub").collection('confirmTrainer')
 
+
+
+
+
+
+// jwt related api 
+app.post('/jwt', async(req, res) =>{
+  const user = req.body 
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '11hr'})
+  res.send({token})
+})
+
+// middleware
+const verifyToken = (req, res, next)=>{
+  console.log('inside verify token', req.headers.authorization)
+ 
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+ 
+}
+
+
+
+
+// verification  admin 
+const verifyAdmin = async(req, res, next) =>{
+  const email = req.decoded.email 
+  const query = {email: email}
+  const user = await usersCollection.findOne(query)
+  console.log(user)
+  const isAdmin = user?.role === 'admin'
+  if(!isAdmin){
+    return res.status(403).send({message: 'forbidden access'})
+  }
+  next()
+}
+
+
+
+// image collection
 app.get('/images',  async(req, res) =>{
  try {
   const result = await imageCollection.find().toArray()
@@ -74,7 +123,7 @@ app.get('/alluser',  async(req, res) =>{
  })
 
 // edits user roll 
-app.patch('/users/role/:id', async( req, res) =>{
+app.patch('/users/role/:id',verifyToken, verifyAdmin, async( req, res) =>{
   try {
     const id = req.params.id 
     const query = {_id: new ObjectId(id)}
@@ -115,7 +164,7 @@ app.post('/betrainer',  async(req, res) =>{
 })
 
 // get the all trainer data 
-app.get('/trainers',  async(req, res) =>{
+app.get('/trainers', verifyToken,  async(req, res) =>{
   try {
    const result = await trainerCollection.find().toArray()
    res.send(result)
@@ -126,7 +175,7 @@ app.get('/trainers',  async(req, res) =>{
  })
  
 // get the all trainer data 
-app.get('/trainers/:id',  async(req, res) =>{
+app.get('/trainers/:id', verifyToken, verifyAdmin,  async(req, res) =>{
   try {
     const id = req.params.id 
   const query = {_id: new ObjectId(id)}
@@ -180,7 +229,7 @@ app.post('/subscriber', async(req, res) =>{
 
 
 // get all subscribers 
-app.get('/allsubscriber', async(req, res) =>{
+app.get('/allsubscriber',verifyToken, verifyAdmin, async(req, res) =>{
   try {
     const result = await subscriberCollection.find().toArray()
     res.send(result)
@@ -192,7 +241,7 @@ app.get('/allsubscriber', async(req, res) =>{
 
 
 // confirm trainer api 
-app.post('/confirm/trainer', async(req, res) =>{
+app.post('/confirm/trainer', verifyToken, verifyAdmin,  async(req, res) =>{
 try {
   const trainer = req.body 
   const result = await confirmTrainerCollection.insertOne(trainer)
